19392dfa3819e47951080ea8e48146a73c782398
nkawaller <kawaller@gmail.com>
2022-02-28 14:36:28 -0500

    wider spacing on timestamp and tag

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material"
 
 const Tag = ({tag}) => {
     return (
-        <Box sx={{marginTop: { xs: 12, sm: 3 }, marginBottom: 1}}>
+        <Box sx={{marginTop: { xs: 12, sm: 3 }, marginBottom: 3}}>
         <Typography variant="caption" sx={{fontFamily: "JetBrains Mono", fontSize: ".694rem", color: "#949494"}}>{tag}</Typography>
         </Box>
     )
--- components/TimeStamp.tsx
+++ components/TimeStamp.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 const TimeStamp = ({ updated }) => {
   return (
     <>
-      <Box sx={{marginBottom: 21}}>
+      <Box sx={{marginBottom: 21, marginTop: 1.5}}>
         <Typography variant="caption" sx={{fontFamily: "JetBrains Mono", fontSize: ".579rem", color: "#949494"}}>{updated}</Typography>
       </Box>
     </>
